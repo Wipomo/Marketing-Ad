3ad728ecd1edc37c614f76af259e2175586d2dc6
Olasubomi Awokoya <olasubomi.awokoya@hotmail.com>
2018-08-16 11:03:52 -0700

    attempting to remove all deleted files in local i.e that are in red when doing a git status by commiting changes after doing a git add -u command to remove multiple deletedfiles in git repository

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname, 'client/public', 'index.html' ));
+   res.sendFile(path.join(__dirname, 'client', 'index.html' ));
 });
 
 if (process.env.NODE_ENV === 'production') {
